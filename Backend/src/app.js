968ad736c74ac6c7c1a194672bb3e06ce1411663
Pihul Jain <pihuljain081224@gmail.com>
2026-08-08 01:42:46 +0530

    fix CORS for production

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-const foodRoutes= require('./routes/food.routes')
-const authRoutes = require('./routes/auth.routes')
-const orderRoutes = require('./routes/order.routes')
-
+const foodRoutes = require("./routes/food.routes");
+const authRoutes = require("./routes/auth.routes");
+const orderRoutes = require("./routes/order.routes");
 
 const app = express();
 
-// Middleware
-    app.use(cors ({
-       origin:"http://localhost:5173",
-       credentials:true,
-    }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://surplr-frontend.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 
-
-app.use(express.json())
+app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
 
-app.use('/auth', authRoutes)
-app.use('/food',foodRoutes)
-app.use('/order',orderRoutes)
-app.use(cors())
+app.use("/auth", authRoutes);
+app.use("/food", foodRoutes);
+app.use("/order", orderRoutes);
 
-module.exports=app;
+module.exports = app;
